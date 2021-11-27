4db78b98373cca87e2f716a7a24cc1b4001cd14a
r14minji <rkdalswl0728@gmail.com>
2021-11-28 00:45:37 +0900

    step10 오류 수정사항

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const btnCall = document.querySelector(".btnCall");
 const menuMo = document.querySelector(".menuMo"); 
+const bgMo = document.querySelector(".bgMo")
 const gnbMo = document.querySelectorAll("#gnb>li");
 
 // 반응형 메뉴버튼
 btnCall.addEventListener("click", e=>{
   e.preventDefault();
-
+  bgMo.classList.toggle("on");
   btnCall.classList.toggle("on");
   menuMo.classList.toggle("on"); 
 })
