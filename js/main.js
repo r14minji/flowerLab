6ba4afdd36e486307498e3d69c711eadbb0c3eac
r14minji <rkdalswl0728@gmail.com>
2021-11-28 00:26:21 +0900

    step10 서브페이지 html, scss, 반응형적용

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const btnCall = document.querySelector(".btnCall");
 const menuMo = document.querySelector(".menuMo"); 
-
+const gnbMo = document.querySelectorAll("#gnb>li");
 
 // 반응형 메뉴버튼
 btnCall.addEventListener("click", e=>{
@@ -10,3 +10,8 @@ btnCall.addEventListener("click", e=>{
   menuMo.classList.toggle("on"); 
 })
 
+gnbMo.forEach( li => {
+  li.addEventListener("click", e => {
+    e.currentTarget.querySelector("div").style.display = "block";
+  })
+})
