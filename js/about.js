038aaa1651cbb9158e79c1f19099ef4c36ae67ca
r14minji <rkdalswl0728@gmail.com>
2021-12-05 02:02:16 +0900

    step18 about 스크롤 돌아올 곳

---
+++ js/about.js
@@ -0,0 +1,17 @@
+// Scroll Animation (sa, 스크롤 애니메이션)
+const saTriggerMargin = 300;
+const saElementList = document.querySelectorAll('.sa');
+console.log(saElementList)
+
+const saFunc = function() {
+  for (const element of saElementList) {
+    if (!element.classList.contains('show')) {
+      if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+        element.classList.add('show');
+      }
+    }
+  }
+}
+
+window.addEventListener('load', saFunc);
+window.addEventListener('scroll', saFunc);
