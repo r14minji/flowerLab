e742f80046952f081ab11ec7e4f73138fd14d323
r14minji <rkdalswl0728@gmail.com>
2021-12-05 02:45:31 +0900

    step18 about 스크롤dataset 오류

--- js/about.js
+++ js/about.js
@@ -1,13 +1,38 @@
+// 클래스 추가
+const H2s = document.querySelectorAll("h2");
+const H3s = document.querySelectorAll("h3");
+const SPANs = document.querySelectorAll("span");
+const IMGs = document.querySelectorAll("img");
+
+addClass(H2s, "sa"), addClass(H2s, "sa-up");
+addClass(H3s, "sa"), addClass(H3s, "sa-up");
+addClass(SPANs, "sa"), addClass(SPANs, "sa-up");
+addClass(IMGs, "sa"), addClass(IMGs, "sa-scaleDown");
+
+function addClass(arr, className){
+  for( let el of arr){
+    el.classList.add(className);
+  }
+}
+
 // Scroll Animation (sa, 스크롤 애니메이션)
 const saTriggerMargin = 300;
+let saTriggerHeight = 0;
 const saElementList = document.querySelectorAll('.sa');
-console.log(saElementList)
 
-const saFunc = function() {
-  for (const element of saElementList) {
-    if (!element.classList.contains('show')) {
-      if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
-        element.classList.add('show');
+
+const saFunc =()=>{
+  for (const el of saElementList) {
+    if (!el.classList.contains('show')) {
+      console.log(dataset.saTrigger);
+      if(el.dataset.saTrigger){
+        saTriggerHeight = document.querySelector(el.dataset.saTrigger).getBoundingClientRect().top + saTriggerMargin;
+      }else{
+        saTriggerHeight = el.getBoundingClientRect().top + saTriggerMargin;
+      }
+
+      if (window.innerHeight > el.getBoundingClientRect().top + saTriggerMargin){
+        el.classList.add('show');
       }
     }
   }
